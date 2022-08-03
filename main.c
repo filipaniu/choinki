127b9948e61e64cfbfac80c95d91554c9e5846ce
Filip <filipporeba967@gmail.com>
2022-08-03 09:53:19 +0200

    first version

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int rysujChoinke(int n) {
+    for (int i = 0; i <= n; ++i) {
+        for (int s = 0; s < n - i; ++s) {
+            printf(" ");
+        }
+        for (int f = 0; f < i + i; ++f) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
+
+int main() {
+    rysujChoinke(7);
+    printf("Hello, World!\n");
+    return 0;
+}
